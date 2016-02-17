284186de0770cb3e174c77af068e64448558b856
Louis Chavane <louis.chavane@gmail.com>
2016-02-17 09:07:07 +0100

    add articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+10.times do
+  a = Article.create(title: Faker::Book.title, content: Faker::Hipster.paragraph)
+  puts "Create article : #{a.title}"
+end
